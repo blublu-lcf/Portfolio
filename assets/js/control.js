7038858a6376bd4d47efa4f0dcaf6cc0b6538845
lcfyan <lcfyan@outlook.com>
2025-01-12 12:10:18 +0800

    Add
    Out of Control

---
+++ assets/js/control.js
@@ -0,0 +1,15 @@
+const navItems = document.querySelectorAll('.nav-item');
+
+const thisitem = document.getElementById("Interaction_Design");
+thisitem.classList.toggle('active')
+
+// 遍历所有导航项，并添加点击事件监听
+navItems.forEach(item => {
+    item.addEventListener('click', function() {
+        // 移除其他nav-item的active类
+        navItems.forEach(nav => nav.classList.remove('active'));
+
+        // 给当前点击的nav-item添加active类
+        this.classList.add('active');
+    });
+});
